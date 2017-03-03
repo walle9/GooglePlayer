6706327a63c85a6d4c49b8500a53e21148ae8069
eve <308821845@qq.com>
2017-03-03 19:21:26 +0800

    提交项目到仓库

--- app/src/main/java/com/walle/googleplayer/MainActivity.java
+++ app/src/main/java/com/walle/googleplayer/MainActivity.java
@@ -1,15 +1,24 @@
 package com.walle.googleplayer;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+import com.astuetz.PagerSlidingTabStrip;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @InjectView(R.id.main_tabs)
+    PagerSlidingTabStrip mainTabs;
+    @InjectView(R.id.main_viewpager)
+    ViewPager mainViewpager;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
@@ -17,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
         initActionBar();
         initview();
         initActionBarDrawerToggle();
